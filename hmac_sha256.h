0f73cc65f44b2f94ef56d142d8028a1f574aa084
Lopeze <pentekee@gmail.com>
2017-02-20 21:58:45 -0800

    follows scattergather approach

--- hmac_sha256.h
+++ hmac_sha256.h
@@ -4,7 +4,6 @@
 #include <stdint.h>
 #include <sys/types.h>
 
-<<<<<<< HEAD
 #include "sha256.h"
 
 struct hmac_sha256_state_struct {
@@ -12,12 +11,8 @@ struct hmac_sha256_state_struct {
   uint8_t inner_key[SHA256_BLOCK_SIZE];
   uint8_t outer_key[SHA256_BLOCK_SIZE];
   uint8_t key[SHA256_DIGEST_SIZE];
-  /* uint8_t digest[SHA256_DIGEST_SIZE]; */
 };
 typedef struct hmac_sha256_state_struct *Hmac_state_t;
-
-
-/* uint8_t *HMAC_SHA256(uint8_t *result, const uint8_t *key, const size_t keysize, const uint8_t *data, const size_t datasize); */
 
 int hmac_sha256_set_key(Hmac_state_t h, const uint8_t *key, size_t key_size);
 
@@ -27,9 +22,4 @@ int hmac_sha256_update(Hmac_state_t h, const uint8_t *data, size_t data_size);
 
 int hmac_sha256_final(uint8_t *digest, Hmac_state_t h);
  
-=======
-
-uint8_t *HMAC_SHA256(uint8_t *result, const uint8_t *key, const size_t keysize, const uint8_t *data, const size_t datasize);
->>>>>>> f23ec8d6e6321f6e2c620d3d105d6b75dd93940f
-
 #endif
